09b6ce0a5e9b037c6c93207d6615e459954de703
shushan <arakelyan.shushan@gmail.com>
2021-05-16 21:30:37 -0700

    Fix typo in unseen verb handling rules

--- parser.py
+++ parser.py
@@ -1,6 +1,5 @@
 import copy
 import logging
-import os
 import signal
 import string
 
@@ -9,7 +8,6 @@ import stanza
 from nltk.ccg import chart, lexicon
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
-from nltk.tag.stanford import StanfordPOSTagger
 from spacy.lang.en import English
 from spacy.tokenizer import Tokenizer
 
@@ -126,11 +124,11 @@ def add_verb(word):
     rules = predicate + " => S/NP {\\x. '@Action'('" + word + "', x)}\n"
     rules += predicate + " => S/PP {\\x. '@Action'('" + word + "', x)}\n"
     rules += predicate + " => (S/NP)/PP {\\y x. '@Action'('" + word + "', x, y)}\n"
-    rules += predicate + " => (S/NP)/NP {\\y x. '@Action'(" + word + ", x, y)}\n"
+    rules += predicate + " => (S/NP)/NP {\\y x. '@Action'('" + word + "', x, y)}\n"
     # rules += predicate + " => S/VP {\\x. '@Action'('" + word + "', x)}\n"
     # rules += predicate + " => (S/VP)/PP {\\y x. '@Action'('" + word + "', x, y)}\n"
-    rules += predicate + " => (S/NP)/VP {\\y x. '@Action'(" + word + ", x, y)}\n"
-    # rules += predicate + " => (S/VP)/NP {\\y x. '@Action'(" + word + ", x, y)}\n"
+    rules += predicate + " => (S/NP)/VP {\\y x. '@Action'('" + word + "', x, y)}\n"
+    # rules += predicate + " => (S/VP)/NP {\\y x. '@Action'('" + word + "', x, y)}\n"
     return predicate, rules
 
 
@@ -431,7 +429,13 @@ if __name__ == "__main__":
     # nltk.download('averaged_perceptron_tagger')
     # stanza.download('en')
     import time
+
     s = time.time()
-    # chart.printCCGDerivation(parse_sentence('how to find the index of a value in 2d array in python?'))
-    parse_sentence("sorting a dictionary by a key")
+    # chart.printCCGDerivation(parse_sentence('remove everything found between instances of start_string and end_string', 100))
+    # chart.printCCGDerivation(parse_sentence("return a list that contains all of the elements in this rdd", 100))
+    chart.printCCGDerivation(parse_sentence("returns an array of bounding boxes of human faces in a image", 100))
+
+    # chart.printCCGDerivation(parse_sentence("use glob to find files recursively"))
+    # tree = parse_sentence("return list sorted by key")
+    # print(getCCGParse(0, tree))
     print("elapsed: ", time.time() - s)
